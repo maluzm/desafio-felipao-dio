5f05f9b58a32ca36857d609ddf0b60dc49c0ee1c
maluzm <maluizavmonteiro@gmail.com>
2024-01-21 20:33:33 -0400

    segundo commit

--- calculadoraDeRankeadas/rankeadas.js
+++ calculadoraDeRankeadas/rankeadas.js
@@ -1,1 +1,31 @@
 // calculadora de rankeadas
+function saldo(vitorias, derrotas){
+	return vitorias - derrotas;
+}
+let saldoRanked = saldo(50, 10)
+
+let nivel = saldoRanked 
+if (nivel <= 10){
+	nivel = "Ferro"
+}
+else if (nivel <= 20){
+	nivel = "Bronze"
+}
+else if (nivel <= 50){
+	nivel = "Prata"
+}
+else if (nivel <= 80){
+	nivel = "Ouro"
+}
+else if (nivel <= 90){
+	nivel = "Diamante"
+}
+else if (nivel <= 100){
+	nivel = "Lendário"
+}
+else {
+	nivel = "Imortal"
+}
+
+
+console.log("O Herói tem de saldo de " + saldoRanked + " está no nível de " + nivel );
